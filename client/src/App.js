0cd28bb3bcf022b43fb3ceb9c748b87efb712552
TPollick <pollick.tyler@gmail.com>
2017-10-18 22:53:32 -0600

    React calls API and displays info.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React    from 'react';
+import Request  from 'superagent';
+import _        from 'lodash';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    var url = "http://localhost:3001/users";
+    Request.get(url).then((response) => {
+      this.setState({
+        names: response.body,
+        total: response.length
+      });
+    });
+  }
+
   render() {
+    var names = _.map(this.state.names, (name) => {
+      return <li key="">{name.name}</li>;
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>{names}</ul>
       </div>
     );
   }
--- server/server.js
+++ server/server.js
@@ -9,12 +9,18 @@ var templateRouter  = require('./src/routes/template');
 // Set the port
 var port = process.env.PORT || 3001;
 
-// Keep this for now. It might help with debuging //
-var router = express.Router();
-router.use(function(req, res, next) {
-  console.log('>> %s %s', req.method, req.path);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Request-With, Content-Type, Accept");
   next();
 });
+
+// Keep this for now. It might help with debuging //
+// var router = express.Router();
+// router.use(function(req, res, next) {
+//   console.log('>> %s %s', req.method, req.path);
+//   next();
+// });
 ////////////////////////////////////////////////////
 
 // ALL API ROUTERS GO HERE
