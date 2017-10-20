71966c1505f227aedaf0677a6bd3e2c899f9c0a7
Kaleb Bickmore <kbic17@gmail.com>
2017-10-20 13:14:20 -0600

    removed quiz-masters, refactored some api stuff

--- server/src/models/quiz-master.js
+++
@@ -1,8 +0,0 @@
-var mongoose    = require('mongoose');
-var QuizSchema      = mongoose.model('Quiz').Schema;
-var UserSchema = new Schema({
-  name: String,
-  password: String,
-  quizID:QuizSchema.Types.ObjectID
-});
-module.exports = mongoose.model('QuizMaster', QuizMasterSchema);
