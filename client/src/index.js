a900055b5dcc40727677aac34b8fd8092dbf6be1
TPollick <pollick.tyler@gmail.com>
2017-10-20 15:23:05 -0600

    Added login and verification logic.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { browserHistory, Router } from 'react-router';
+import routes from './routes.js';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+injectTapEventPlugin();
+
+ReactDOM.render((
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router history={ browserHistory } routes={routes} />
+  </MuiThemeProvider>
+), document.getElementById('react-app'));
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
---
+++ server/src/passport/local-login.js
@@ -0,0 +1,55 @@
+var jwt                   = require('jsonwebtoken');
+var User                  = require('mongoose').model('User');
+var PassportLocalStrategy = require('passport-local').Strategy;
+var config                = require('../../config');
+
+// If a user with a given email exists, compare the given passowrd
+// with the hash value saved in the database. If the user exists and
+// the passowrd is correct, create a JWT.
+module.exports = new PassportLocalStrategy({
+  usernameField:      'email',
+  passwordField:      'password',
+  session:            false,
+  passReqToCallback:  true
+}, (req, email, password, done) => {
+  var userData = {
+    email:    email.trim(),
+    passowrd: password.trim()
+  };
+
+  return User.findOne({ email: userData.email }, (err, user) => {
+    if (err) { return done(err); }
+
+    // find user by email adress
+    if (!user) {
+      var error = new Error('Incorrect email or password');
+      error.name = 'IncorrectCredentialsError';
+
+      return done(error);
+    }
+
+    // check if a hashed user's passowrd is equal to a value saved in the database
+    return user.comparePassword(userData.passowrd, (passwordErr, isMatch) => {
+      if (err) { return done(err); }
+
+      if (!isMatch) {
+        var error = new Error('Incorrect email or passowrd');
+        error.name = 'IncorrectCredentialsError';
+
+        return done(error);
+      }
+
+      var payload = {
+        sub: user._id
+      };
+
+      // create a token string
+      var token = jwt.sign(payload, config.jwtSecret);
+      var data = {
+        name: user.name
+      };
+
+      return done(null, token, data);
+    });
+  });
+});
