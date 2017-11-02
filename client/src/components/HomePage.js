3523b40c036e730c9e2c18ad1c7629b85eae7226
Caleb Maughan <cmaughan1582@gmail.com>
2017-11-01 20:28:21 -0600

    Added Text field and button...they don't do anything yet though

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -4,6 +4,12 @@ import { Card, CardTitle } from 'material-ui/Card';
 const HomePage = () => (
   <Card className='container'>
     <CardTitle title="React Application" subtitle="This is the home page."/>
+    <div>
+      <TextField
+        floatingLabelText = "Quiz ID"
+        name="id"
+        >
+    </div>
   </Card>
 );
 
