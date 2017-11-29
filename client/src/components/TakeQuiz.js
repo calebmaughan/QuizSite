4fd4e31a5a1231c26d82011076d31e6d5d1a82e9
Dick Loveland <richard.k.loveland@gmail.com>
2017-11-28 21:06:36 -0700

    tried to accomodate merge

--- client/src/components/TakeQuiz.js
+++ client/src/components/TakeQuiz.js
@@ -1,22 +1,23 @@
-import React from 'react';
+imporimport React from 'react';
 import { Card, CardTitle } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 
 
 const TakeQuiz = ({
-    quiz
+    quiz,
+    answers
 
 }) =>(
-  <Card className="container" style={{borderRadius: '25px'}}>
-    <CardTitle className="title" title = {quiz.id} titleColor = "#E4EEFF"/>
+  <Card className="container">
+    <CardTitle title = {quiz.id}/>
     <form>
     <div>
-      <RaisedButton className = "answer" type = 'submit' label = 'answer 1' primary/>
-      <RaisedButton className = "answer" type = 'submit' label = 'answer 2' primary/>
+      <RaisedButton className = "answer" type = 'submit' label = {answers.a1} primary/>
+      <RaisedButton className = "answer" type = 'submit' label = {answers.a2} primary/>
     </div>
     <div>
-    <RaisedButton className = "answer" type = 'submit' label = 'answer 3' primary/>
-    <RaisedButton className = "answer" type = 'submit' label = 'answer 4' primary/>
+    <RaisedButton className = "answer" type = 'submit' label = {answers.a3} primary/>
+    <RaisedButton className = "answer" type = 'submit' label = {answers.a4} primary/>
     </div>
     </form>
   </Card>
