b70865ade6a85dd4c9cfebb6fe5a2fc8d7f44227
Dick Loveland <richard.k.loveland@gmail.com>
2017-11-28 21:18:24 -0700

    This should merge now!

--- client/src/components/TakeQuiz.js
+++ client/src/components/TakeQuiz.js
@@ -12,21 +12,12 @@ const TakeQuiz = ({
     <CardTitle title = {quiz.id}/>
     <form>
     <div>
-<<<<<<< HEAD
       <RaisedButton className = "answer" type = 'submit' label = {answers.a1} primary/>
       <RaisedButton className = "answer" type = 'submit' label = {answers.a2} primary/>
     </div>
     <div>
     <RaisedButton className = "answer" type = 'submit' label = {answers.a3} primary/>
     <RaisedButton className = "answer" type = 'submit' label = {answers.a4} primary/>
-=======
-      <RaisedButton className = "answer" type = 'submit' label = {answers.a1}/>
-      <RaisedButton className = "answer" type = 'submit' label = {answers.a2}/>
-    </div>
-    <div>
-    <RaisedButton className = "answer" type = 'submit' label = {answers.a3}/>
-    <RaisedButton className = "answer" type = 'submit' label = {answers.a4}/>
->>>>>>> master
     </div>
     </form>
   </Card>
