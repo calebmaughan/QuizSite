b4263c7172178def05a7fdf8fbc52741e5ae6cbc
Caleb Maughan <cmaughan1582@gmail.com>
2017-11-30 20:05:25 -0700

    changed to onclick

--- client/src/components/TakeQuiz.js
+++ client/src/components/TakeQuiz.js
@@ -11,16 +11,14 @@ const TakeQuiz = ({
 }) =>(
   <Card className="container">
     <CardTitle title = {quiz.id}/>
-    <form onSubmit={next}>
     <div>
-      <RaisedButton className = "answer" type = 'submit' label = {answers.a1} primary/>
-      <RaisedButton className = "answer" type = 'submit' label = {answers.a2} primary/>
+      <RaisedButton className = "answer" onClick={next} label = {answers.a1} primary/>
+      <RaisedButton className = "answer" onClick={next} label = {answers.a2} primary/>
     </div>
     <div>
-    <RaisedButton className = "answer" type = 'submit' label = {answers.a3} primary/>
-    <RaisedButton className = "answer" type = 'submit' label = {answers.a4} primary/>
+    <RaisedButton className = "answer" onClick={next} label = {answers.a3} primary/>
+    <RaisedButton className = "answer" onClick={next} label = {answers.a4} primary/>
     </div>
-    </form>
   </Card>
 
 )
