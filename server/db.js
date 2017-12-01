8b1454f24ecc358694b832a215a1e84d0b1c2358
Kaleb Bickmore <kbic17@gmail.com>
2017-11-30 19:57:18 -0700

    changin databases

--- server/db.js
+++ server/db.js
@@ -2,8 +2,8 @@ var mongoose  = require('mongoose');
 var config    = require('./config/index')
 
 // The URI we will be connecting to. Change this as needed.
-//var dbURI = 'mongodb://tpollick:apassword@ds117485.mlab.com:17485/live-polling-test';
-var dbURI = 'mongodb://kbic17:apassword@ds227525.mlab.com:27525/trckblmp';
+var dbURI = 'mongodb://tpollick:apassword@ds117485.mlab.com:17485/live-polling-test';
+//var dbURI = 'mongodb://kbic17:apassword@ds227525.mlab.com:27525/trckblmp';
 
 //<<<<<<< HEAD
 // var dbURI = 'mongodb://cmaughan1582:password@ds125016.mlab.com:25016/practice'
