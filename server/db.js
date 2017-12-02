4f498972149aefc5dda8835a1ddfaf3b524adeb6
Caleb Maughan <cmaughan1582@gmail.com>
2017-12-01 17:28:22 -0700

    moved the db

--- server/db.js
+++ server/db.js
@@ -4,9 +4,9 @@ var config    = require('./config/index')
 // The URI we will be connecting to. Change this as needed.
 
 
-var dbURI = 'mongodb://cmaughan1582:password@ds125016.mlab.com:25016/practice'
+//var dbURI = 'mongodb://cmaughan1582:password@ds125016.mlab.com:25016/practice'
 
-//var dbURI = 'mongodb://tpollick:apassword@ds117485.mlab.com:17485/live-polling-test';
+var dbURI = 'mongodb://tpollick:apassword@ds117485.mlab.com:17485/live-polling-test';
 
 //var dbURI = 'mongodb://kbic17:apassword@ds227525.mlab.com:27525/trckblmp';
 
